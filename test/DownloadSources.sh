a512a57d77ce1a420131219114308ec924150a34
Victor Fernandez <victor.fernandez@bsc.es>
2019-05-08 14:48:23 +0200

    Added commands to decompress the downloaded vcf's

--- test/DownloadSources.sh
+++ test/DownloadSources.sh
@@ -6,4 +6,7 @@ curl -O -J -L ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/NIST_NA12878_HG
 curl -O -J -L -u "gsapubftp-anonymous":"" ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/Mills_and_1000G_gold_standard.indels.b37.vcf.gz
 curl -O -J -L -u "gsapubftp-anonymous":"" ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/dbsnp_138.b37.vcf.gz
 
+gunzip Mills_and_1000G_gold_standard.indels.b37.vcf.gz
+gunzip dbsnp_138.b37.vcf.gz
+
 curl -O -J -L ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
